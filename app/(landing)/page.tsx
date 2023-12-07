cdb0fca8afa7f8a0e8d89d9dd5e0979289a7147e
imomaikel <imomaikeldev@gmail.com>
2023-12-07 17:44:52 +0100

    Added auth functionality

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,5 +1,21 @@
 'use client';
+import { Button } from '@/shared/components/ui/button';
+import { signIn } from 'next-auth/react';
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <Button
+        onClick={() =>
+          signIn('discord', {
+            redirect: true,
+            callbackUrl: '/dashboard',
+          }).catch(() => {})
+        }
+      >
+        Login
+      </Button>
+    </div>
+  );
 }
--- app/dashboard/_assets/components/Navbar.tsx
+++ app/dashboard/_assets/components/Navbar.tsx
@@ -11,13 +11,16 @@ import { FiChevronsDown } from 'react-icons/fi';
 import { useCart } from '@/hooks/use-cart';
 import { useTheme } from 'next-themes';
 import Image from 'next/image';
-import Link from 'next/link';
 import { toast } from 'sonner';
+import Link from 'next/link';
+import { signOut, useSession } from 'next-auth/react';
+import { Skeleton } from '@/shared/components/ui/skeleton';
 
 const Navbar = () => {
   const { onOpen: openMobileSidebar } = useMobileSidebar();
   const { onOpen: openShoppingCart } = useCart();
   const { setTheme } = useTheme();
+  const { data } = useSession();
 
   const updateCurrency = () => {
     toast.info('Currency updated');
@@ -48,17 +51,19 @@ const Navbar = () => {
               <PopoverTrigger className="group">
                 <div className="flex items-center mr-2">
                   {/* SSR */}
-                  <div>imomaikel</div>
+                  <div className="max-w-[200px] truncate hidden md:block">
+                    {!data?.user ? <Skeleton className="w-[100px] h-6" /> : data.user.name}
+                  </div>
                   <FiChevronsDown className="w-6 h-6 md:mr-1 group-data-[state=open]:rotate-180 group-data-[state=open]:text-primary transition-all group-hover:text-primary" />
                   <Avatar className="w-8 h-8 hidden md:block">
-                    <AvatarImage src="https://cdn.discordapp.com/embed/avatars/4xx.png" />
+                    <AvatarImage src={data?.user?.image ?? 'https://cdn.discordapp.com/embed/avatars/3.png'} />
                     <AvatarFallback className="w-8 h-8 relative">
                       <Image src="https://cdn.discordapp.com/embed/avatars/3.png" alt="avatar" fill sizes="" />
                     </AvatarFallback>
                   </Avatar>
                 </div>
               </PopoverTrigger>
-              <PopoverContent className="flex flex-col">
+              <PopoverContent className="flex flex-col mr-2">
                 <div className="mb-1">Theme</div>
                 <div className="flex space-x-2">
                   <Button variant="secondary" onClick={() => setTheme('dark')}>
@@ -84,7 +89,26 @@ const Navbar = () => {
                   </SelectContent>
                 </Select>
                 <Separator className="my-2" />
-                <Button variant="ghost">Log out</Button>
+                <div className="flex md:hidden flex-col">
+                  <div className="flex justify-between">
+                    <div className="flex flex-col">
+                      <div className="mb-1">Logged in as</div>
+                      <div className="max-w-[200px] truncate">{data?.user?.name}</div>
+                    </div>
+                    <div>
+                      <Avatar className="w-11 h-11">
+                        <AvatarImage src={data?.user?.image ?? 'https://cdn.discordapp.com/embed/avatars/3.png'} />
+                        <AvatarFallback className="w-11 h-11 relative">
+                          <Image src="https://cdn.discordapp.com/embed/avatars/3.png" alt="avatar" fill sizes="" />
+                        </AvatarFallback>
+                      </Avatar>
+                    </div>
+                  </div>
+                  <Separator className="my-2" />
+                </div>
+                <Button variant="ghost" onClick={() => signOut()}>
+                  Log out
+                </Button>
               </PopoverContent>
             </Popover>
             {/* SSR */}
