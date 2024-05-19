394dcac1452570ddb228f7f7e039bc56eaf1f25b
imomaikel <imomaikeldev@gmail.com>
2024-05-19 23:42:45 +0200

    Fixed steam joinlinks

--- app/join/[queryPort]/route.ts
+++ app/join/[queryPort]/route.ts
@@ -1,8 +1,12 @@
+import prisma from '../../_shared/lib/prisma';
 import { redirect } from 'next/navigation';
 import { NextRequest } from 'next/server';
 
-const handler = (req: NextRequest, { params }: { params: { queryPort: string } }) => {
-  return redirect(`steam://connect/join.elitecore.nl:${params.queryPort}`);
+const handler = async (req: NextRequest, { params: { queryPort } }: { params: { queryPort: string } }) => {
+  const config = await prisma.config.findFirst();
+  const ipAddr = config?.ipAddress;
+
+  return redirect(`steam://run/346110//+connect%20${ipAddr}:${queryPort}`);
 };
 
 export { handler as GET };
